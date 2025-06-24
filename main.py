b119bbf0045b778cad06c9a2fa125384579a30e9
John Zetterman <jzetterman@users.noreply.github.com>
2025-06-24 13:15:11 -0400

    Allow for user input as an argument when calling main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,10 +7,15 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
+    prompt_string = sys.argv
+    if len(sys.argv) < 2:
+        print("No prompt was provided, try again")
+        sys.exit(1)
+
     client = genai.Client(api_key=api_key)
     prompt = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=prompt_string
     )
     print(prompt.text)
     print(f"Prompt tokens: {prompt.usage_metadata.prompt_token_count}")
